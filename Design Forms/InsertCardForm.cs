ed2a78f03778f2df88864cc93c1dec851118a6ff
softixed <softixed@gmail.com>
2024-04-22 00:55:17 +0300

    git commit

---
+++ Design Forms/InsertCardForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM_Winforms.Design_Forms
+{
+    public partial class InsertCardForm : Form
+    {
+        public InsertCardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InsertCardBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InsertCardForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
