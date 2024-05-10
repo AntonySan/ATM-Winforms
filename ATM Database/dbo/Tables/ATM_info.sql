de5c2d7047ffc198d9e4ad7cf30bd7aeab2bad81
AntonSan <anton.sanatos00@gmail.com>
2024-05-10 09:13:11 +0300

    git commit

--- ATM Database/dbo/Tables/ATM_info.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[ATM_info] (
-    [Id]          INT           IDENTITY (1, 1) NOT NULL,
-    [Card_number] VARCHAR (MAX) NOT NULL,
-    [PIN]         VARCHAR (MAX) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
